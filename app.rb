337aa466796de5660b084b3507e22a9ff2a8d04f
Alex MacCaw <maccman@gmail.com>
2012-04-18 23:49:33 -0700

    new password

--- app.rb
+++ app.rb
@@ -52,7 +52,8 @@ class App < Sinatra::Base
   end
 
   use Rack::Auth::Basic, "Protected Area" do |username, password|
-    username == 'dragon' && password == 'slayer'
+    (username == 'dragon' && password == 'slayer') ||
+      (username == 'stylo' && password == 'password1')
   end
 
   get '/' do
